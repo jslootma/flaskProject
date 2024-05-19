452a332eba19a52bb695a4dd6933b19ad012ec13
beezyStudents <Jens.Slootmans@kuleuven.be>
2024-05-19 20:27:33 +0200

    update audio scripts + audio folder structure

--- audio-script.py
+++ audio-script.py
@@ -1,10 +1,167 @@
 import requests  # pip install requests
 
-detected_class = input("Enter the detected class: ")  # Replace this code with the script that defines the detected class [the api accepts Bee, Hornet, Other]
 
-url = f"http://127.0.0.1:5000/audio-detected/{detected_class}"  # URL to add detected class to the database, notice that 127.0.0.1 is the localhost IP address
+### Code to classify the recorded WAV files
+import os
+import shutil
+from datetime import datetime
+#from pushbullet import Pushbullet
+import csv
+import librosa
+import numpy as np
+#import trained SVM model
+from joblib import load
+date_list = []
+time_list = []
 
-response = requests.post(url)  # POST request to add detected class to the database
+timestamp_csv_file = "timestamp.csv"
+# Specify the directories
+input_directory = "/home/beezyStudents/Documents/flaskProject/media/WAV/"
+output_directory = "/home/beezyStudents/Documents/Timestamps_and_labels"  # Output directory won't be necessary, immediately write to database
+hornet_list = []
+# Specify your Pushbullet API key
+#pushbullet_api_key = "YOUR_PUSHBULLET_API_KEY"
+model_path = "/home/beezyStudents/Documents/flaskProject/static/audio_models"
 
-print(response.status_code) # 201 if successful added, 400 if invalid class
-print(response.json()) # {"message": "Detected class registered"} if successful added, {"message": "Invalid class detected"} if invalid class
+def process_wav_files(input_directory, output_directory):
+
+    # Get a list of all files in the specified directory
+    files= sorted(os.listdir(input_directory))
+    
+    # Filter out only the WAV files
+    wav_files = [file for file in files if file.endswith(".wav")]
+
+   
+    # Iterate over each WAV file
+    for wav_file in wav_files:
+        # Process the WAV file
+        output = process_single_wav(os.path.join(input_directory, wav_file))
+        print(output)
+        # If the output is equal to 'hornet', save timestamp and label in a new directory
+        if output == 'Hornet':
+    #split title into time and date to add in different columns  
+            date_list = []
+            time_list = []
+            parts = wav_file.split("_")
+            date_part = parts[0]
+            date_part = date_part[4:]
+            date_list.append(date_part)
+        
+            time_part = parts[1]
+            time_part = time_part[:-4]
+            time_list.append(time_part)
+    #add to csv file
+            write_to_csv(timestamp_csv_file, date_list, time_list)
+            # Delete the WAV file after processing
+            os.remove(os.path.join(input_directory, wav_file))
+        else:
+            os.remove(os.path.join(input_directory, wav_file))
+    
+            
+def write_to_csv(csv_file, date_list, time_list):
+#data to CSV in append mode
+    with open(csv_file, 'a', newline='\n') as csvfile:
+        writer = csv.writer(csvfile)
+    #write in new row
+        for date, time in zip(date_list, time_list):
+            writer.writerow([date, time])
+
+def process_single_wav(wav_filepath):
+   
+    # Load the trained SVM model
+    svm_model = load(f'{model_path}/svm_model_label.joblib')
+    
+    #load in file
+    print(wav_filepath)
+    audio, sr = librosa.load(wav_filepath)
+    
+    #preprocess sound
+    # Filter
+
+    from scipy.signal import butter, filtfilt
+
+    def butter_bandpass(data, lowcut, highcut, fs, order=5):
+        """
+        Design a bandpass filter.
+
+        Args:
+        - lowcut (float) : the low cutoff frequency of the filter.
+        - highcut (float): the high cutoff frequency of the filter.
+        - fs     (float) : the sampling rate.
+        - order    (int) : order of the filter, by default defined to 5.
+        """
+        
+        # calculate the Nyquist frequency
+        nyq = 0.5 * fs
+        
+        # design filter
+        low = lowcut / nyq
+        high = highcut / nyq
+        b, a = butter(order, [low, high], btype='band', analog=False)
+        
+        # returns the filter coefficients: numerator and denominator
+        y = filtfilt(b, a, data)
+        return y
+
+    # Define the bandpass frequencies
+    lowcut = 100  # Lower cutoff frequency
+    highcut = 1000  # Higher cutoff frequency
+
+    # Apply bandpass filter to the audio
+    yf = butter_bandpass(audio, lowcut, highcut, sr, order=5)
+        
+    mfccs_features = librosa.feature.mfcc(y = yf, sr = sr, n_mfcc = 20)
+    mfccs_scaled_features = np.mean(mfccs_features.T, axis=0)
+    mfccs_scaled_features = mfccs_scaled_features.reshape(-1, 20)
+    
+    audio_features = mfccs_scaled_features
+    
+    #run classifier
+    # Predict class label for the audio sample
+    predicted_label = svm_model.predict(audio_features)
+    
+    print(predicted_label)
+    
+    return predicted_label
+
+    
+
+def save_timestamp_and_label(wav_filename, file):
+    # Create the output directory if it doesn't exist
+    
+    # Get the current timestamp
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    # Create a new file with the timestamp and label
+    label = 'hornet'
+    new_filename = f"{timestamp}_{label}.txt"
+    with open(os.path.join(output_directory, new_filename), 'w') as file:
+        file.write(f"Timestamp: {timestamp}\nLabel: {label}")
+
+#def send_push_notification():
+    # Initialize Pushbullet with your API key
+    #pb = Pushbullet(pushbullet_api_key)
+    # Send a push notification with the timestamp and label
+    #timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    #title = "Hornet Detected!"
+    #body = f"Hornet detected at {timestamp}"
+    #pb.push_note(title, body)
+
+# Call the function to process WAV files in the input directory
+process_wav_files(input_directory, output_directory)
+
+
+
+
+
+
+
+
+
+#detected_class = input("Enter the detected class: ")  # Replace this code with the script that defines the detected class [the api accepts Bee, Hornet, Other]
+
+#url = f"http://127.0.0.1:5000/audio-detected/{detected_class}"  # URL to add detected class to the database, notice that 127.0.0.1 is the localhost IP address
+
+#response = requests.post(url)  # POST request to add detected class to the database
+
+#print(response.status_code) # 201 if successful added, 400 if invalid class
+#print(response.json()) # {"message": "Detected class registered"} if successful added, {"message": "Invalid class detected"} if invalid class
---
+++ record_audio.py
@@ -0,0 +1,69 @@
+import os
+import numpy as np
+from gpiozero import MCP3008
+import time
+import wave
+import signal
+import pandas as pd
+from datetime import datetime
+
+# Specify the directory to save the recordings
+WAV_dir = "/home/beezyStudents/Documents/flaskProject/media/WAV"
+
+
+# Specify the channel on the ADC
+mic = MCP3008(channel=0)
+sample_rate = 48000  # 48 kHz
+record_duration = 5  # 5-second recording duration
+
+continue_recording = True
+
+
+def end_recording(signal, frame):
+    global continue_recording
+    print("Ctrl+C captured, ending recording.")
+    continue_recording = False
+
+
+signal.signal(signal.SIGINT, end_recording)
+
+
+def get_init_recording_number():
+	files = os.listdir(WAV_dir)
+	files = [file for file in files if file.endswith('.wav')]
+	return len(files) + 1
+
+
+def record_and_save():
+	recording_number = get_init_recording_number()
+	while continue_recording:
+		samples = []
+		start_time = time.time()
+		while time.time() < start_time + record_duration:
+			samples.append(mic.value)
+		print("Recording ended")
+		actual_sample_rate = len(samples) / record_duration
+		print(f"Actual sample rate: {actual_sample_rate} samples per second")
+		
+		timestring = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+		
+		wav_filename = f"{recording_number:08}.{timestring}.wav"
+		wav_filepath = os.path.join(WAV_dir, wav_filename)
+		
+		samples_arr = np.array(samples)
+		num_frames = len(samples_arr)
+		scaled_data = np.int16(samples_arr * 32767)
+		with wave.open(wav_filepath, 'w') as wavfile:
+			wavfile.setparams((1, 2, actual_sample_rate, num_frames, 'NONE', 'not compressed'))
+			wavfile.writeframes(scaled_data.tobytes())
+		print(f"WAV file #{recording_number} created")
+		recording_number += 1
+
+# Ensure the output directory exists
+os.makedirs(WAV_dir, exist_ok=True)
+
+# Main function to initiate recording
+try:
+    record_and_save()
+except KeyboardInterrupt:
+    pass  # Handle Ctrl+C KeyboardInterrupt
